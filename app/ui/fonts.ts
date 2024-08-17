fdf0d9b981012e7d25e32f06bb53539a87f6c9e5
otsuka0302 <ys.training231129@gmail.com>
2024-08-17 12:50:57 +0900

    GL-1 : chapter6までの変更のまとめ

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const Lusitania = Lusitana({
+  subsets: ["latin"],
+  weight: ["700"],
+});
